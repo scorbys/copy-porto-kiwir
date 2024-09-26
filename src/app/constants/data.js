c3a3f661885a162e3feafd4b011d0105b4985a13
Lancea <61652470+blazertooth-99@users.noreply.github.com>
2024-09-26 07:44:43 +0000

    Edit heigth layout business & add news layout

---
+++ src/app/constants/data.js
@@ -0,0 +1,23 @@
+import Newsimg from "../assets/news-1.jpg"
+
+
+export const NEWS = [
+    {
+        date: '14 Agustus 2024',
+        readTime: '3 menit baca',
+        title: 'Agree Perluas Pasar Kopi Indonesia sampai ke Belanda',
+        Image: Newsimg,
+    },
+    {
+        date: '05 Agustus 2024',
+        readTime: '3 menit baca',
+        title: 'Kerja Sama Strategis Telkom dan Scaia Jepang Dorong Inovasi Pertanian Demi Keberlanjutan',
+        image: Newsimg,
+    },
+    {
+        date: '15 Juli 2024',
+        readTime: '4 menit baca',
+        title: 'Budi Daya Petani Serai Wangi Lebih Efisien Berkat Teknologi IoT dari Agree',
+        image: Newsimg,
+    },
+];
--- src/app/landing_page/business.tsx
+++ src/app/landing_page/business.tsx
@@ -13,10 +13,10 @@ const Business = (props: any) => {
         </div>
       </div>
       <div className="p-0">
-        <div className="grid grid-cols-1 md:grid-cols-[25%_50%_25%] md:col-span-3 justify-center items-center gap-4 px-20 border-2 border-black">
+        <div className="grid grid-cols-1 grid-rows-[auto] md:grid-rows-[fit_1fr_auto] md:grid-cols-[25%_50%_25%] md:col-span-3 justify-center items-center gap-4 px-0 md:px-20">
 
           {/*START GRID VIEW BUSINESS 1 */}
-          <div className="relative w-full h-full row-span-1 md:row-span-2 border-2 border-black my-0 py-0">
+          <div className="relative w-full h-full row-span-1 md:row-span-2 my-0 py-0">
             <div className="h-full bg-teal-700 text-white p-0 relative">
               <div className="p-0 mt-auto">
                 <Image
@@ -47,15 +47,16 @@ const Business = (props: any) => {
           </div>
 
           {/* START GRID VIEW BUSINESS 2 */}
-          <div className="relative border-2 border-black">
-            <div className="bg-sky-600 text-white p-0 relative">
-              <div className="p-0 mt-auto">
+          <div className="my-0 py-0 bg-sky-600 text-white">
+            <div className="w-full grid grid-cols-1 md:grid-cols-2">
+              <div className="h-full right-0 object-right p-0 relative">
                 <Image
                   src={tes}
                   alt="Business 2 background"
-                  className="object-cover md:w-full w-full h-auto md:h-40"
+                  className="relative object-cover bottom-0 md:w-[80%] w-full h-auto md:h-full"
                 />
               </div>
+              <div className="col-span-1">
               <div className="flex items-center justify-center mt-4">
                 <Avatar className="mb-1 size-20">
                   <AvatarImage src="https://github.com/shadcn.png" />
@@ -74,10 +75,11 @@ const Business = (props: any) => {
               </div>
             </div>
           </div>
+          </div>
           {/* END GRID VIEW BUSINESS 2 */}
 
           {/* START GRID VIEW BUSINESS 3 */}
-          <div className="relative w-full h-full row-span-1 md:row-span-2 border-2 border-black">
+          <div className="relative w-full h-full row-span-1 md:row-span-2">
             <div className="object-right h-full bg-red-300 text-white p-0 relative">
               <div className="p-0 mt-auto">
                 <Image
@@ -109,7 +111,7 @@ const Business = (props: any) => {
           {/* END GRID VIEW BUSINESS 3 */}
 
           {/* START GRID VIEW BUSINESS 4 */}
-          <div className="relative border-2 border-black">
+          <div className="relative border-2">
             <div className=" h-full bg-red-300 text-white p-0 relative">
               <div className="p-0 mt-auto">
                 <Image
@@ -140,8 +142,8 @@ const Business = (props: any) => {
           {/* END GRID VIEW BUSINESS 4 */}
 
           {/* START GRID VIEW BUSINESS 5 */}
-          <div className="col-span-1 md:col-span-3 bg-green-600 text-white border-2 border-black">
-            <div className="w-full grid grid-cols-1 md:grid-cols-2   border-2 border-red" >
+          <div className="col-span-1 md:col-span-3 bg-green-600 text-white">
+            <div className="w-full grid grid-cols-1 md:grid-cols-2" >
             <div className="h-full left-0 object-left-top p-0 relative">
               <Image
                 src={tes}
@@ -156,13 +158,13 @@ const Business = (props: any) => {
                 </Avatar>
                 <div>
                   <h2 className="text-2xl font-bold mb-2">BUSINESS 5</h2>
-                  <p className="mb-4 px-1">
+                  <p className="mb-2 px-1">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua.
                   </p>
                   <div className="py-5 flex gap-4">
-                  <button className="border border-white px-4 py-2 rounded hover:bg-white hover:text-green-600 transition-colors">
+                  <button className="border border-white px-4 py-0 rounded hover:bg-white hover:text-green-600 transition-colors">
                     LEARN MORE
                   </button>
                   </div>
