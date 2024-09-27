529f08b03475a4be4943312f69580fe88c92e251
Tom <tomyanjasp@gmail.com>
2024-09-27 16:26:15 +0800

    link navbar

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = (props: any) => {
     <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
       <div className="w-full max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-14 items-center">
-          <Link href="#" className="flex items-center" prefetch={false}>
+          <Link href="/" className="flex items-center" prefetch={false}>
             <Image
             {...props}
                src={logo}
@@ -51,35 +51,35 @@ const Navbar = (props: any) => {
           </Link>
           <nav className="hidden md:flex gap-4">
             <Link
-              href="#"
+              href="/landing_page/business"
               className="font-medium flex items-center text-sm transition-colors hover:underline"
               prefetch={false}
             >
               Business
             </Link>
             <Link
-              href="#"
+              href="/landing_page/career"
               className="font-medium flex items-center text-sm transition-colors hover:underline"
               prefetch={false}
             >
               Career
             </Link>
             <Link
-              href="#"
+              href="/landing_page/news"
               className="font-medium flex items-center text-sm transition-colors hover:underline"
               prefetch={false}
             >
               News
             </Link>
             <Link
-              href="#"
+              href="/landing_page/about"
               className="font-medium flex items-center text-sm transition-colors hover:underline"
               prefetch={false}
             >
               About
             </Link>
             <Link
-              href="#"
+              href="/landing_page/contact"
               className="font-medium flex items-center text-sm transition-colors hover:underline"
               prefetch={false}
             >
